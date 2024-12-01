6fa302281d736f96165ea79ebebcd787a157325b
Ridhcheall Doherty <132768875+hillister@users.noreply.github.com>
2024-12-01 14:55:40 +0000

    Add js files

---
+++ day1/index.js
@@ -0,0 +1,8 @@
+const data1 = [3, 4, 2, 1, 3, 3]
+
+const data2 = [4, 3, 5, 3, 9, 3]
+
+data1.sort(function(a, b){return a - b})
+data2.sort(function(a, b){return a - b})
+
+console.log(data1, data2)
